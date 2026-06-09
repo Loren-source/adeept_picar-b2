06fe25775b8660794696ec91f94ded1b1bfd5981
Gaspard-M <gaspard.martouzet@gmail.com>
2026-06-09 13:29:54 +0200

    Update 08_lineTracking.py

--- prog/08_lineTracking.py
+++ prog/08_lineTracking.py
@@ -1,28 +1,36 @@
 import time
-import argparse
 from gpiozero import InputDevice
 
-line_pin_left = 22
-line_pin_middle = 27
-line_pin_right = 17
 
-left = InputDevice(pin=line_pin_right)
-middle = InputDevice(pin=line_pin_middle)
-right = InputDevice(pin=line_pin_left)
+class LineSensor:
+    """Gère les 3 capteurs de ligne IR d'un robot."""
 
-def run():
-    status_right = right.value
-    status_middle = middle.value
-    status_left = left.value
-    print('left: %d   middle: %d   right: %d' %(status_right,status_middle,status_left))
+    def init(self, pin_left=22, pin_middle=27, pin_right=17):
+        # Les pins deviennent des attributs de l'objet
+        self.left   = InputDevice(pin=pin_left)
+        self.middle = InputDevice(pin=pin_middle)
+        self.right  = InputDevice(pin=pin_right)
+
+    def read(self):
+        """Retourne un dictionnaire avec l'état des 3 capteurs."""
+        return {
+            "left":   self.left.value,
+            "middle": self.middle.value,
+            "right":  self.right.value,
+        }
+
+    def print_status(self):
+        """Affiche l'état des capteurs dans le terminal."""
+        values = self.read()
+        print("left: %(left)d   middle: %(middle)d   right: %(right)d" % values)
 
 
-if __name__ == '__main__':
+if __name__ == "main":
+    sensor = LineSensor()          # On instancie l'objet une seule fois
+
     try:
-      while 1:
-        run()
-        time.sleep(0.3)
+        while True:
+            sensor.print_status()  # On appelle la méthode sur l'objet
+            time.sleep(0.3)
     except KeyboardInterrupt:
         pass
-
-
