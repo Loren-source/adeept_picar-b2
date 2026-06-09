6234d5f02a3eb3f9385fcbf9c7f3960a86aa36ea
JustinPelpoir <justin.pelpoir@efrei.net>
2026-06-09 12:21:30 +0200

    POO adaptation of line tracking task

--- examples/08_lineTracking.py
+++ examples/08_lineTracking.py
@@ -2,27 +2,37 @@ import time
 import argparse
 from gpiozero import InputDevice
 
-line_pin_left = 22
-line_pin_middle = 27
-line_pin_right = 17
+import time
+from gpiozero import InputDevice
 
-left = InputDevice(pin=line_pin_right)
-middle = InputDevice(pin=line_pin_middle)
-right = InputDevice(pin=line_pin_left)
+class LineSensor:
 
-def run():
-    status_right = right.value
-    status_middle = middle.value
-    status_left = left.value
-    print('left: %d   middle: %d   right: %d' %(status_right,status_middle,status_left))
+    def __init__(self, pin_left=22, pin_middle=27, pin_right=17):
+        # Les pins deviennent des attributs de l'objet
+        self.left   = InputDevice(pin=pin_left)
+        self.middle = InputDevice(pin=pin_middle)
+        self.right  = InputDevice(pin=pin_right)
+
+    def read(self):
+        """Retourne un dictionnaire avec l'état des 3 capteurs."""
+        return {
+            "left":   self.left.value,
+            "middle": self.middle.value,
+            "right":  self.right.value,
+        }
+
+    def print_status(self):
+        """Affiche l'état des capteurs dans le terminal."""
+        values = self.read()
+        print("left: %(left)d   middle: %(middle)d   right: %(right)d" % values)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    sensor = LineSensor()          # On instancie l'objet une seule fois
+
     try:
-      while 1:
-        run()
-        time.sleep(0.3)
+        while True:
+            sensor.print_status()  # On appelle la méthode sur l'objet
+            time.sleep(0.3)
     except KeyboardInterrupt:
         pass
-
-
