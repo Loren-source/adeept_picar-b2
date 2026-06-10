55e99540b68306f5d06191777544d7cda37dc14a
corentin-verrier <151041160+corentin-verrier@users.noreply.github.com>
2026-06-10 16:13:40 +0200

    avance

--- prog/avancer-distance-check.py
+++ prog/avancer-distance-check.py
@@ -27,7 +27,7 @@ try:
 
     while True:
         if movement == "M":
-            RobotMotor.drive_with_ramp(1, 60, 5)
+            motor.drive_with_ramp(1, 60, 5)
 
             while True:                          # surveillance continue
                 distance = ultrasonic.get_distance()
