e5f9601379a9233fd55d921a18854a89e950ac4a
corentin-verrier <151041160+corentin-verrier@users.noreply.github.com>
2026-06-11 18:11:49 +0200

    avance

--- prog/suivi-ligne.py
+++ prog/suivi-ligne.py
@@ -22,28 +22,37 @@ try:
             servos.set_angle(0, angle)  # réinitialise l'angle à chaque boucle
             if ((status['left']== 0) and (status['middle'] == 0) and (status['right'] == 0)):
                 motor.forward_slow()
+                print("Pas de ligne détectée, avance lentement")
             elif ((status['left']== 1) and (status['middle'] == 0) and (status['right'] == 0)):
                 servos.set_angle(0, angle + 25)
                 motor.forward_slow()
+                print("Détection à gauche, tourne à gauche")
             elif ((status['left']== 0) and (status['middle'] == 0) and (status['right'] == 1)):
                 servos.set_angle(0, angle - 25)
                 motor.forward_slow()
+                print("Détection à droite, tourne à droite")
             elif ((status['left']== 0) and (status['middle'] == 1) and (status['right'] == 0)):
                 motor.forward_slow()
+                print("Détection au centre, avance droit")
             elif ((status['left']== 1) and (status['middle'] == 1) and (status['right'] == 0)):
                 servos.set_angle(0, angle - 25)
                 motor.forward_slow()
+                print("Détection à gauche et au centre, tourne à gauche")
             elif ((status['left']== 0) and (status['middle'] == 1) and (status['right'] == 1)):
                 servos.set_angle(0, angle + 25)
                 motor.forward_slow()
+                print("Détection à droite et au centre, tourne à droite")
             elif ((status['left']== 1) and (status['middle'] == 0) and (status['right'] == 1)):
                 motor.forward_slow()
+                print("Détection à gauche et à droite, avance droit")
             elif ((status['left']== 1) and (status['middle'] == 1) and (status['right'] == 1)):
                 angle = 98
                 servos.set_angle(0, angle)
                 motor.backward_slow()
+                print("Détection à gauche, au centre et à droite, recule lentement")
             else :
                 motor.backward_slow()
+                print("Situation inattendue, recule lentement")
             if distance < 200:
                 motor.stop()
                 movement = input("Envoie M pour redémarrer : ")
