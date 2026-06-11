b25b77da214456d3c460e3e1a148f0665fa38ef1
corentin-verrier <151041160+corentin-verrier@users.noreply.github.com>
2026-06-11 17:52:24 +0200

    avance

--- prog/suivi-ligne.py
+++ prog/suivi-ligne.py
@@ -14,58 +14,36 @@ try:
 
     while True:
         movement = input("Appuie sur M pour démarrer : ")
-
-        if movement == 'M' or movement == 'm':
-            motor.drive_with_ramp(30, 1, 5)
-
-        while movement == 'M' or movement == 'm':
+        while movement == 'M' or 'm' :
             status = tracker.get_status()
-            distance = ultrasonic.get_distance()
             tracker.print_status()
-
-            if ((status['right'] == 1) and (status['left'] == 1) and (status['middle'] == 1)):
+            distance = ultrasonic.get_distance()
+            if ((status['left']== 0) and (status['middle'] == 0) and (status['right'] == 0)):
+                motor.forward_slow()
+            elif ((status['left']== 1) and (status['middle'] == 0) and (status['right'] == 0)):
+                servos.set_angle(0, angle + 25)
+                motor.set_motor(1, 20)
+            elif ((status['left']== 0) and (status['middle'] == 0) and (status['right'] == 1)):
+                servos.set_angle(0, angle - 25)
+                motor.set_motor(1, 20)
+            elif ((status['left']== 0) and (status['middle'] == 1) and (status['right'] == 0)):
+                motor.forward_slow()
+            elif ((status['left']== 1) and (status['middle'] == 1) and (status['right'] == 0)):
+                servos.set_angle(0, angle - 25)
+                motor.backward_slow()
+            elif ((status['left']== 0) and (status['middle'] == 1) and (status['right'] == 1)):
+                servos.set_angle(0, angle + 25)
+                motor.backward_slow()
+            elif ((status['left']== 1) and (status['middle'] == 0) and (status['right'] == 1)):
+                motor.forward_slow()
+            elif ((status['left']== 1) and (status['middle'] == 1) and (status['right'] == 1)):
                 angle = 98
                 servos.set_angle(0, angle)
-                motor.drive_with_ramp(30, 1, 0)
-                status = tracker.get_status()
-
-            elif ((status['right'] == 1) and (status['left'] == 0) and (status['middle'] == 1)):
-                servos.set_angle(0, angle + 20)
-                while ((status['right'] == 1) and (status['middle'] == 1)):
-                    motor.drive_with_ramp(20, 1, 0)
-                    status = tracker.get_status()
-
-            elif ((status['right'] == 1) and (status['left'] == 0) and (status['middle'] == 0)):
-                servos.set_angle(0, angle - 20)
-                while ((status['right'] == 1)):
-                    motor.drive_with_ramp(30, -1, 0)
-                    status = tracker.get_status()
-
-            elif ((status['right'] == 0) and (status['left'] == 1) and (status['middle'] == 1)):
-                servos.set_angle(0, angle + 20)
-                while ((status['left'] == 1) and (status['middle'] == 1)):
-                    motor.drive_with_ramp(30, 1, 0)
-                    status = tracker.get_status()
-
-            elif ((status['right'] == 0) and (status['left'] == 1) and (status['middle'] == 0)):
-                servos.set_angle(0, angle - 20)
-                while status['right'] == 1:
-                    motor.stop()
-                    motor.drive_with_ramp(30, -1, 0)
-                    status = tracker.get_status()
-
-            elif ((status['right'] == 0) and (status['left'] == 0) and (status['middle'] == 1)):
-                servos.set_angle(0, angle)
-                while ((status['right'] != 0) or (status['left'] != 0) or (status['middle'] != 0)):
-                    angle = 98
-                    motor.stop()
-                    motor.drive_with_ramp(30, -1, 0)
-                    status = tracker.get_status()
-
+                motor.backward_slow()
             if distance < 200:
                 motor.stop()
                 movement = input("Envoie M pour redémarrer : ")
-                break  # sort de la surveillance
+                break
 
 except KeyboardInterrupt:
     print("Fin du programme")
