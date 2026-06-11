6e70dde745c660c09809845e0e1243b874c911cb
corentin-verrier <151041160+corentin-verrier@users.noreply.github.com>
2026-06-11 17:02:33 +0200

    avance

--- prog/suivi-ligne.py
+++ prog/suivi-ligne.py
@@ -20,44 +20,44 @@ try:
                 status = tracker.get_status()
                 distance = ultrasonic.get_distance()
                 tracker.print_status()
-                if ((status['right'] == 1) and (status['left'] == 1) and (status['middle'] == 1)):
+                if ((status['right'] == 0) and (status['left'] == 0) and (status['middle'] == 0)):
                     angle = 98
                     servos.set_angle(0, angle)
                     motor.drive_with_ramp(30, 1, 0)
                     status = tracker.get_status()
 
 
-                elif ((status['right'] == 1) and (status['left'] == 0) and (status['middle'] == 1)):
-                    while ((status['left'] == 0) and (status['middle'] == 0)):
+                elif ((status['right'] == 0) and (status['left'] == 1) and (status['middle'] == 0)):
+                    while ((status['left'] == 1) and (status['middle'] == 1)):
                         servos.set_angle(0, angle + 20)
                         motor.drive_with_ramp(20, 1, 0)
                         status = tracker.get_status()
 
 
-                elif ((status['right'] == 1) and (status['left'] == 0) and (status['middle'] == 0)):
-                    while ((status['left'] == 0) and (status['middle'] == 0)):
+                elif ((status['right'] == 0) and (status['left'] == 1) and (status['middle'] == 1)):
+                    while ((status['left'] == 1) and (status['middle'] == 1)):
                         target = servos.set_angle(0, angle - 20)
                         motor.drive_with_ramp(20, -1, 0)
                         status = tracker.get_status()
 
 
-                elif ((status['right'] == 0) and (status['left'] == 1) and (status['middle'] == 1)):
-                    while (status['right'] == 0):
+                elif ((status['right'] == 1) and (status['left'] == 0) and (status['middle'] == 0)):
+                    while (status['right'] == 1):
                         target = servos.set_angle(0, angle + 20)
                         motor.drive_with_ramp(20, 1, 0)
                         status = tracker.get_status()
 
 
-                elif ((status['right'] == 0) and (status['left'] == 1) and (status['middle'] == 0)):
-                    while status['right'] == 0:
+                elif ((status['right'] == 1) and (status['left'] == 0) and (status['middle'] == 1)):
+                    while status['right'] == 1:
                         servos.set_angle(0, angle - 20)
                         motor.stop()
                         motor.drive_with_ramp(20, -1, 0)
                         status = tracker.get_status()
 
 
-                elif ((status['right'] == 0) and (status['left'] == 0) and (status['middle'] == 1)):
-                    while ((status['right'] != 1) or (status['left'] != 1) or (status['middle'] != 1)):
+                elif ((status['right'] == 1) and (status['left'] == 1) and (status['middle'] == 0)):
+                    while ((status['right'] != 0) or (status['left'] != 0) or (status['middle'] != 0)):
                         angle = 98
                         servos.set_angle(0, angle)
                         motor.stop()
