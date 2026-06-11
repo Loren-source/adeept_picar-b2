7d1b9c54570cacc0d1a339b14839a687d130b205
corentin-verrier <151041160+corentin-verrier@users.noreply.github.com>
2026-06-11 11:07:36 +0200

    avance

--- prog/suivi-ligne.py
+++ prog/suivi-ligne.py
@@ -1,11 +1,48 @@
-import time
-import threading
 from lineTracking import LineTracker
-from Spi_WS2812 import LED
 from motor import RobotMotor
-import buzzer
+from servo import RobotServos
 
 try:
+    motor = RobotMotor()
+    tracker = LineTracker()
+    servos = RobotServos()
+    angle = 90
     while True :
-        motor      = RobotMotor()
-        buzzer      = buzzer.Buzzer()
+        if movement == "M":
+            motor.drive_with_ramp(20, 1, 5)
+            distance1 = ultrasonic.get_distance()
+            print(f"Distance : {distance1:.2f} mm")
+            time.sleep(0.05)
+
+            status = tracker.get_status()
+            if status['right'] == 0:
+                motor.stop()
+                servos.set_angle(angle - 10)
+                motor.drive_with_ramp(20,1, 1)
+            if status['right'] == 0:
+                motor.stop()
+                servos.set_angle(angle + 10)
+                motor.drive_with_ramp(20,1, 1)
+
+        while True:  # surveillance continue
+            distance = ultrasonic.get_distance()
+            print(f"Distance : {distance:.2f} mm")
+            time.sleep(0.05)
+
+        if distance < 200:
+            motor.stop()
+            movement = input("Envoie M pour redémarrer : ")
+            break  # sort de la surveillance
+except :
+    print("Fin de programme par Ctrl-C")
+
+finally:
+    ultrasonic.close()  # libère GPIO24
+    motor.stop()
+    print("Nettoyage final réalisé")
+
+
+
+
+
+
