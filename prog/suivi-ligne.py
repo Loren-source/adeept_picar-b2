823e2bf33827f5ed63e81c7065a2dad6e421f3c5
corentin-verrier <151041160+corentin-verrier@users.noreply.github.com>
2026-06-11 12:07:02 +0200

    avance

--- prog/suivi-ligne.py
+++ prog/suivi-ligne.py
@@ -13,36 +13,31 @@ try:
     while True :
         movement = input("Appuie sur M pour démarrer : ")
         if movement == "M":
-            motor.drive_with_ramp(20, 1, 5)
-            distance1 = ultrasonic.get_distance()
-            print(f"Distance : {distance1:.2f} mm")
-            time.sleep(0.05)
-
-            status = tracker.get_status()
-            if status['right'] == 1:
-                motor.stop()
-                servos.set_angle(0,angle - 20)
-                motor.drive_with_ramp(20,1, 1)
-            if status['left'] == 1:
-                motor.stop()
-                servos.set_angle(0,angle + 20)
-                motor.drive_with_ramp(20,1, 1)
-
-        while True:  # surveillance continue
-            distance = ultrasonic.get_distance()
-            tracker.print_status()
-            time.sleep(0.05)
-
-            if distance < 200:
-                motor.stop()
-                movement = input("Envoie M pour redémarrer : ")
-                break  # sort de la surveillance
+            while True :
+                motor.drive_with_ramp(20, 1, 5)
+                status = tracker.get_status()
+                distance = ultrasonic.get_distance()
+                tracker.print_status()
+                time.sleep(0.05)
+                if status['right'] == 0:
+                    motor.stop()
+                    servos.set_angle(0,angle - 20)
+                    motor.drive_with_ramp(20,1, 1)
+                if status['left'] == 0:
+                    motor.stop()
+                    servos.set_angle(0,angle + 20)
+                    motor.drive_with_ramp(20,1, 1)
+                if distance < 200:
+                    motor.stop()
+                    movement = input("Envoie M pour redémarrer : ")
+                    break  # sort de la surveillance
 except :
     print("Fin de programme par Ctrl-C")
 
 finally:
     ultrasonic.close()  # libère GPIO24
     motor.stop()
+    servos.centrer_servos(0)
     print("Nettoyage final réalisé")
 
 
