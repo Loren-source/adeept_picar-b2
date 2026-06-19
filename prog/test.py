e61011d8b2fe2d0402b460b9958de6197c7bedba
Loren-source <loren.koudoukpo@efrei.net>
2026-06-19 13:37:55 +0200

    test.py

--- prog/test.py
+++ prog/test.py
@@ -1,14 +1,17 @@
 import time
 from motor import RobotMotor
 from servo import RobotServos
+from ultra import Ultrasonic
 
 robot = RobotMotor()
 servos = RobotServos()
+ultra = Ultrasonic()
 
 servos.set_angle(0, 127)
 
 try:
     while True:
+        distance = ultra.get_distance()
         robot.set_motor(1, 30)
         time.sleep(0.05)
 except KeyboardInterrupt:
