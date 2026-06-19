ec406804cb414f8d94807daa7e65a8a10347bd0d
Loren-source <loren.koudoukpo@efrei.net>
2026-06-19 14:57:37 +0200

    tache11.py

--- prog/tache11.py
+++ prog/tache11.py
@@ -10,171 +10,146 @@ ultra = Ultrasonic()
 tracker = LineTracker()
 servos = RobotServos()
 
+ANGLE_CENTRE=97
+ANGLE_GAUCHE_LEGER=92
+ANGLE_GAUCHE_FORT=85
+ANGLE_DROITE_LEGER=102
+ANGLE_DROITE_FORT=110
+ANGLE_RECH_GAUCHE=70
+ANGLE_RECH_DROITE=120
 
-ANGLE_CENTRE = 97
-ANGLE_GAUCHE_LEGER = 92
-ANGLE_GAUCHE_FORT  = 85
+VITESSE_DROITE=55
+VITESSE_CORRECTION=40
+VITESSE_VIRAGE=28
+VITESSE_RECHERCHE=20
+DISTANCE_STOP=200
 
-ANGLE_DROITE_LEGER = 102
-ANGLE_DROITE_FORT  = 110
+actif=False
+etat="SUIVI"
+angle_actuel=None
+derniere_direction="centre"
 
-ANGLE_RECH_GAUCHE = 70
-ANGLE_RECH_DROITE = 120
+etat_prec=(1,1,1)
+virage_confirme=False
 
-VITESSE_DROITE = 45
-VITESSE_CORRECTION = 35
-VITESSE_VIRAGE = 25
-VITESSE_RECHERCHE = 20
-
-DISTANCE_STOP = 200      # mm
-
-actif = False
-etat = "SUIVI"
-angle_actuel = None
-derniere_direction = "centre"
-
-# permet d'éviter les faux passages en RECHERCHE
-compteur_000 = 0
-
-def braquer(angle):
+def braquer(a):
     global angle_actuel
-    if angle_actuel != angle:
-        servos.set_angle(0, angle)
-        angle_actuel = angle
-        
-def avancer(angle, vitesse):
-    braquer(angle)
-    robot.set_motor(1, vitesse)
-    
-def reculer(angle, vitesse):
-    braquer(angle)
-    robot.set_motor(-1, vitesse)
+    if angle_actuel!=a:
+        servos.set_angle(0,a)
+        angle_actuel=a
 
-def ecouter_clavier():
+def avance(a,v):
+    braquer(a)
+    robot.set_motor(1,v)
+
+def recule(a,v):
+    braquer(a)
+    robot.set_motor(-1,v)
+
+def clavier():
     global actif
     while True:
-        cmd = input().strip().upper()
-        if cmd == "M":
-            actif = True
+        c=input().strip().upper()
+        if c=="M":
+            actif=True
             robot.stop_feux()
-            print("= DEMARRAGE =")
-        elif cmd == "A":
-            actif = False
+            print("Demarrage")
+        elif c=="A":
+            actif=False
             robot.stopper()
-            print("== ARRET ==")
-threading.Thread(
-    target=ecouter_clavier,
-    daemon=True
-).start()
+            print("Arret")
 
+threading.Thread(target=clavier,daemon=True).start()
 
 try:
     while True:
+
         if not actif:
             time.sleep(0.02)
             continue
-        distance = ultra.get_distance()
 
-        if distance < DISTANCE_STOP:
-            print("Obstacle détecté")
+        if ultra.get_distance()<DISTANCE_STOP:
             robot.stop()
-            actif = False
+            actif=False
             continue
-        capteurs = tracker.get_status()
-        l = capteurs["left"]
-        m = capteurs["middle"]
-        r = capteurs["right"]
-        print(f"{l}{m}{r}   Etat={etat}")
-        if etat == "RECHERCHE":
-            if derniere_direction == "gauche":
-                reculer(
-                    ANGLE_RECH_GAUCHE,
-                    VITESSE_RECHERCHE
-                )
-            elif derniere_direction == "droite":
-                reculer(
-                    ANGLE_RECH_DROITE,
-                    VITESSE_RECHERCHE
-                )
-            else:
-                reculer(
-                    ANGLE_CENTRE,
-                    VITESSE_RECHERCHE
-                )
-            if (l, m, r) == (1,1,1):
 
-                print(">>> Ligne retrouvée")
+        s=tracker.get_status()
+        etat_cap=(s["left"],s["middle"],s["right"])
+        l,m,r=etat_cap
+
+        if etat=="RECHERCHE":
+
+            if derniere_direction=="gauche":
+                recule(ANGLE_RECH_GAUCHE,VITESSE_RECHERCHE)
+            elif derniere_direction=="droite":
+                recule(ANGLE_RECH_DROITE,VITESSE_RECHERCHE)
+            else:
+                recule(ANGLE_CENTRE,VITESSE_RECHERCHE)
+
+            if etat_cap==(1,1,1):
                 robot.stopper()
                 braquer(ANGLE_CENTRE)
-                time.sleep(0.05)
-                etat = "REALIGNEMENT"
+                etat="REALIGNEMENT"
+
             time.sleep(0.02)
             continue
 
-        if etat == "REALIGNEMENT":
-            avancer(
-                ANGLE_CENTRE,
-                25
-            )
-            time.sleep(0.12)
-            etat = "SUIVI"
+        if etat=="REALIGNEMENT":
+            avance(ANGLE_CENTRE,30)
+            time.sleep(0.15)
+            etat="SUIVI"
+            virage_confirme=False
+            etat_prec=(1,1,1)
             continue
-        
-        if (l,m,r) == (1,1,1):
-            compteur_000 = 0
-            derniere_direction = "centre"
-            avancer(
-                ANGLE_CENTRE,
-                VITESSE_DROITE
-            )
-        elif (l,m,r) == (1,1,0):
 
-            compteur_000 = 0
+        # confirmation des virages
+        if etat_prec==(1,1,1):
+            if etat_cap in [(1,1,0),(0,1,1)]:
+                virage_confirme=False
 
-            derniere_direction = "gauche"
+        elif etat_prec==(1,1,0) and etat_cap==(1,0,0):
+            virage_confirme=True
 
-            avancer(
-                ANGLE_GAUCHE_LEGER,
-                VITESSE_CORRECTION
-            )
+        elif etat_prec==(0,1,1) and etat_cap==(0,0,1):
+            virage_confirme=True
 
-        elif (l,m,r) == (1,0,0):
-            compteur_000 = 0
-            derniere_direction = "gauche"
-            avancer(
-                ANGLE_GAUCHE_FORT,
-                VITESSE_VIRAGE
-            )
+        # suivi
+        if etat_cap==(1,1,1):
+            virage_confirme=False
+            derniere_direction="centre"
+            avance(ANGLE_CENTRE,VITESSE_DROITE)
 
-        # Robot trop à gauche
-        elif (l,m,r) == (0,1,1):
-            compteur_000 = 0
-            derniere_direction = "droite"
-            avancer(
-                ANGLE_DROITE_LEGER,
-                VITESSE_CORRECTION
-            )
-        elif (l,m,r) == (0,0,1):
-            compteur_000 = 0
-            derniere_direction = "droite"
-            avancer(
-                ANGLE_DROITE_FORT,
-                VITESSE_VIRAGE
-            )
+        elif etat_cap==(1,1,0):
+            derniere_direction="gauche"
+            if virage_confirme:
+                avance(ANGLE_GAUCHE_LEGER,32)
+            else:
+                avance(ANGLE_GAUCHE_LEGER,VITESSE_CORRECTION)
 
-        # Ligne perdue
-        elif (l,m,r) == (0,0,0):
-            compteur_000 += 1
-            # évite un faux déclenchement
-            if compteur_000 >= 2:
-                print(">>> Ligne perdue")
-                etat = "RECHERCHE"
-        else:
-            compteur_000 = 0
+        elif etat_cap==(1,0,0):
+            derniere_direction="gauche"
+            avance(ANGLE_GAUCHE_FORT,VITESSE_VIRAGE)
+
+        elif etat_cap==(0,1,1):
+            derniere_direction="droite"
+            if virage_confirme:
+                avance(ANGLE_DROITE_LEGER,32)
+            else:
+                avance(ANGLE_DROITE_LEGER,VITESSE_CORRECTION)
+
+        elif etat_cap==(0,0,1):
+            derniere_direction="droite"
+            avance(ANGLE_DROITE_FORT,VITESSE_VIRAGE)
+
+        elif etat_cap==(0,0,0):
+            etat="RECHERCHE"
+
+        etat_prec=etat_cap
         time.sleep(0.02)
+
 except KeyboardInterrupt:
-    print("Fin du programme")
+    pass
+
 finally:
     robot.stopper()
     braquer(ANGLE_CENTRE)
-    print("Robot arrêté proprement.")
