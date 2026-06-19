bb3e85f6e86d560f1a60a14e613e0653e33ee249
Loren-source <loren.koudoukpo@efrei.net>
2026-06-19 13:38:59 +0200

    test.py

--- prog/test.py
+++ prog/test.py
@@ -1,17 +1,17 @@
 import time
 from motor import RobotMotor
 from servo import RobotServos
-from ultra import Ultrasonic
+from lineTracking import LineTracker
 
 robot = RobotMotor()
 servos = RobotServos()
-ultra = Ultrasonic()
+tracker = LineTracker()
 
 servos.set_angle(0, 127)
 
 try:
     while True:
-        distance = ultra.get_distance()
+        capteurs = tracker.get_status()
         robot.set_motor(1, 30)
         time.sleep(0.05)
 except KeyboardInterrupt:
