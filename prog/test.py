9f671b7d3963313757d08ab278dbe676736e342d
Loren-source <loren.koudoukpo@efrei.net>
2026-06-19 13:36:30 +0200

    test.py

--- prog/test.py
+++ prog/test.py
@@ -1,13 +1,16 @@
+import time
 from motor import RobotMotor
 from servo import RobotServos
-import time
 
 robot = RobotMotor()
 servos = RobotServos()
 
 servos.set_angle(0, 127)
-time.sleep(0.5)
 
-robot.set_motor(1, 30)
-time.sleep(2)
-robot.stopper()
+try:
+    while True:
+        robot.set_motor(1, 30)
+        time.sleep(0.05)
+except KeyboardInterrupt:
+    robot.stopper()
+    servos.set_angle(0, 97)
