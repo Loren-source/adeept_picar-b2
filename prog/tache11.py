4fd001c11efa873e680e00b14f3b360186d2268f
Loren-source <loren.koudoukpo@efrei.net>
2026-06-22 15:48:58 +0200

    tache11.py

--- prog/tache11.py
+++ prog/tache11.py
@@ -1,45 +1,39 @@
 #!/usr/bin/env python3
 
 import time
-import sys
 
-sys.path.append('/home/loren/adeept_picar-b2/prog')
-
-import motor
 from servo import RobotServos
-import lineTracking
+from motor import RobotMotor
+from lineTracking import LineTracker
 
 
-# ==========================
-# INITIALISATION
-# ==========================
-
-servos = RobotServos()
-
-
-# ==========================
+# =====================
 # REGLAGES
-# ==========================
+# =====================
 
 CENTRE = 97
 
-# tes valeurs mesurées
+# tes vraies limites testées
 GAUCHE_MAX = 125
 DROITE_MAX = 70
 
 GAUCHE = 118
 DROITE = 76
 
-
 V_LIGNE = 35
-V_VIRAGE = 45
+V_VIRAGE = 28
+
+
+# =====================
+# INITIALISATION
+# =====================
+
+servos = RobotServos()
+moteur = RobotMotor()
+tracker = LineTracker()
 
 dernier_angle = CENTRE
 
-
-# ==========================
-# SERVO
-# ==========================
 
 def tourner(angle):
     global dernier_angle
@@ -49,107 +43,95 @@ def tourner(angle):
         dernier_angle = angle
 
 
-# ==========================
-# MOTEURS
-# ==========================
-
-def avancer(vitesse):
-    motor.motor_left(
-        status=1,
-        direction=1,
-        speed=vitesse
-    )
-
-    motor.motor_right(
-        status=1,
-        direction=1,
-        speed=vitesse
-    )
+def avancer(angle, vitesse):
+    tourner(angle)
+    moteur.set_motor(1, vitesse)
 
 
 def stop():
-    motor.motorStop()
+    moteur.stopper()
+    tourner(CENTRE)
 
-
-# ==========================
-# PROGRAMME
-# ==========================
 
 print("START")
+
 
 try:
 
     tourner(CENTRE)
     time.sleep(1)
 
-
     while True:
 
-        L, M, R = lineTracking.readLine()
+        s = tracker.get_status()
+
+        L = int(s["left"])
+        M = int(s["middle"])
+        R = int(s["right"])
 
         print((L, M, R))
 
 
-        # ====================
-        # MILIEU
-        # ====================
+        # ==========================
+        # LIGNE AU MILIEU
+        # ==========================
 
         if (L, M, R) == (1,1,1):
 
-            tourner(CENTRE)
-            avancer(V_LIGNE)
+            avancer(CENTRE, V_LIGNE)
 
 
-        # ====================
-        # PART A DROITE
-        # ligne vue à gauche
-        # ====================
+        # ==========================
+        # PETITE CORRECTION GAUCHE
+        # ==========================
 
         elif (L, M, R) == (0,1,1):
 
-            tourner(DROITE)
-            avancer(V_VIRAGE)
+            avancer(DROITE, V_LIGNE)
 
 
         elif (L, M, R) == (0,0,1):
 
-            tourner(DROITE_MAX)
-            avancer(V_VIRAGE)
+            # gros virage droite
+            avancer(DROITE_MAX, V_VIRAGE)
 
 
-        # ====================
-        # PART A GAUCHE
-        # ligne vue à droite
-        # ====================
+        # ==========================
+        # PETITE CORRECTION DROITE
+        # ==========================
 
         elif (L, M, R) == (1,1,0):
 
-            tourner(GAUCHE)
-            avancer(V_VIRAGE)
+            avancer(GAUCHE, V_LIGNE)
 
 
         elif (L, M, R) == (1,0,0):
 
-            tourner(GAUCHE_MAX)
-            avancer(V_VIRAGE)
+            # gros virage gauche
+            avancer(GAUCHE_MAX, V_VIRAGE)
 
 
-        # ====================
-        # PERDU
-        # garde dernier angle
-        # ====================
+        # ==========================
+        # PERTE DE LIGNE
+        # ==========================
 
-        elif (L, M, R) == (0,0,0):
+        elif (L,M,R) == (0,0,0):
 
-            avancer(30)
+            # NE PLUS FONCER TOUT DROIT
+            # il garde le dernier angle
+            moteur.set_motor(1,20)
 
 
-        time.sleep(0.03)
+        else:
+
+            avancer(CENTRE,25)
+
+
+        time.sleep(0.04)
 
 
 
 except KeyboardInterrupt:
 
-    tourner(CENTRE)
-    stop()
     print("FIN")
+    stop()
