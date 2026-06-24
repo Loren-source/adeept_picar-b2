036fe4503a0145248b8406c025fccf8d2115beeb
Loren-source <loren.koudoukpo@efrei.net>
2026-06-24 14:19:27 +0200

    tache11.py

--- prog/tache11.py
+++ prog/tache11.py
@@ -1,170 +1,41 @@
-import time
+# ========================
+# Robot trop à droite
+# corriger GAUCHE
+# ========================
 
-from motor import RobotMotor
-from servo import RobotServos
-from lineTracking import LineTracker
+elif etat == (1,1,0):
 
+    dernier_cote = -1
 
-robot = RobotMotor()
-servo = RobotServos()
-tracker = LineTracker()
+    tourner(GAUCHE_LEGER)
+    robot.set_motor(1,VITESSE_VIRAGE)
 
 
-# =========================
-# REGLAGES PICAR-B
-# =========================
+elif etat == (1,0,0):
 
-CENTRE = 97
+    dernier_cote = -1
 
-GAUCHE_LEGER = 80
-GAUCHE_FORT = 55
+    tourner(GAUCHE_FORT)
+    robot.set_motor(1,VITESSE_RECUP)
 
-DROITE_LEGER = 115
-DROITE_FORT = 140
 
 
-VITESSE_RAPIDE = 45
-VITESSE_VIRAGE = 32
-VITESSE_RECUP = 25
+# ========================
+# Robot trop à gauche
+# corriger DROITE
+# ========================
 
+elif etat == (0,1,1):
 
-angle_actuel = CENTRE
-dernier_cote = 0
-# -1 gauche
-#  1 droite
+    dernier_cote = 1
 
+    tourner(DROITE_LEGER)
+    robot.set_motor(1,VITESSE_VIRAGE)
 
-# =========================
-# SERVO FLUIDE
-# =========================
 
-def tourner(cible):
+elif etat == (0,0,1):
 
-    global angle_actuel
+    dernier_cote = 1
 
-    # filtre pour éviter les coups secs
-    angle_actuel = angle_actuel*0.65 + cible*0.35
-
-    servo.set_angle(0, angle_actuel)
-
-
-
-# =========================
-# BOUCLE PRINCIPALE
-# =========================
-
-print("START")
-
-servo.set_angle(0, CENTRE)
-robot.set_motor(1, 30)
-
-time.sleep(1)
-
-
-try:
-
-    while True:
-
-
-        data = tracker.get_status()
-
-        L = data["left"]
-        M = data["middle"]
-        R = data["right"]
-
-        etat = (L,M,R)
-
-        print(etat)
-
-
-
-        # ========================
-        # Pleine ligne noire
-        # ========================
-
-        if etat == (1,1,1):
-
-            tourner(CENTRE)
-            robot.set_motor(1,VITESSE_RAPIDE)
-
-
-
-        # ========================
-        # Ligne part à gauche
-        # ========================
-
-        elif etat == (1,1,0):
-
-            dernier_cote = -1
-
-            tourner(GAUCHE_LEGER)
-            robot.set_motor(1,VITESSE_VIRAGE)
-
-
-
-        elif etat == (1,0,0):
-
-            dernier_cote = -1
-
-            tourner(GAUCHE_FORT)
-            robot.set_motor(1,VITESSE_RECUP)
-
-
-
-
-        # ========================
-        # Ligne part à droite
-        # ========================
-
-        elif etat == (0,1,1):
-
-            dernier_cote = 1
-
-            tourner(DROITE_LEGER)
-            robot.set_motor(1,VITESSE_VIRAGE)
-
-
-
-        elif etat == (0,0,1):
-
-            dernier_cote = 1
-
-            tourner(DROITE_FORT)
-            robot.set_motor(1,VITESSE_RECUP)
-
-
-
-        # ========================
-        # Ligne perdue
-        # ========================
-
-        elif etat == (0,0,0):
-
-            robot.set_motor(1,22)
-
-
-            if dernier_cote == -1:
-
-                tourner(GAUCHE_FORT)
-
-
-            elif dernier_cote == 1:
-
-                tourner(DROITE_FORT)
-
-
-            else:
-
-                tourner(CENTRE)
-
-
-
-        time.sleep(0.02)
-
-
-
-except KeyboardInterrupt:
-
-    robot.stopper()
-
-    servo.set_angle(0,CENTRE)
+    tourner(DROITE_FORT)
+    robot.set_motor(1,VITESSE_RECUP)
