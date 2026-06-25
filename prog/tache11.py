5f14c6447296ce834a827840a4048f053d0b8276
Loren-source <loren.koudoukpo@efrei.net>
2026-06-25 16:14:17 +0200

    tache11.py

--- prog/tache11.py
+++ prog/tache11.py
@@ -13,18 +13,17 @@ tracker = LineTracker()
 
 
 # ==========================
-# REGLAGES VALIDES
+# REGLAGES VALIDES VIRAGES
 # ==========================
 
 CENTRE = 97
 
 
-# petites corrections
+# NE PAS TOUCHER
 GAUCHE_LEGER = 112
 DROITE_LEGER = 82
 
 
-# vrais virages
 GAUCHE_FORT = 128
 DROITE_FORT = 65
 
@@ -33,6 +32,7 @@ DROITE_FORT = 65
 VITESSE_LIGNE = 34
 VITESSE_APPROCHE = 27
 VITESSE_VIRAGE = 18
+VITESSE_POINTILLE = 32
 VITESSE_PERDU = 14
 
 
@@ -40,14 +40,11 @@ angle_actuel = CENTRE
 
 
 dernier_sens = 0
-# 1 = gauche
-# -1 = droite
-
 
 compteur_virage = 0
+compteur_perdu = 0
 
 dernier_etat = (1,1,1)
-compteur_perdu = 0
 
 
 
@@ -60,6 +57,7 @@ def tourner(cible):
     global angle_actuel
 
 
+    # NE PAS TOUCHER
     angle_actuel = angle_actuel*0.6 + cible*0.4
 
 
@@ -109,56 +107,75 @@ try:
 
 
         # =====================
-        # LIGNE CENTREE
+        # TOUT DROIT
         # =====================
 
         if etat == (1,1,1):
+
 
             compteur_virage = 0
             compteur_perdu = 0
 
 
             cible = CENTRE
+
             vitesse = VITESSE_LIGNE
 
 
 
 
+
         # =====================
-        # VIRAGE GAUCHE
+        # GAUCHE
         # =====================
 
         elif etat == (1,1,0):
 
+
             compteur_perdu = 0
+
             compteur_virage += 1
+
 
             dernier_sens = 1
 
 
+
             if compteur_virage > 2:
 
+
                 cible = GAUCHE_FORT
+
                 vitesse = VITESSE_VIRAGE
+
 
             else:
 
+
                 cible = GAUCHE_LEGER
+
                 vitesse = VITESSE_APPROCHE
+
+
 
 
 
 
         elif etat == (1,0,0):
 
+
             compteur_perdu = 0
 
+
             compteur_virage += 2
+
 
             dernier_sens = 1
 
 
+
             cible = GAUCHE_FORT
+
 
             vitesse = VITESSE_VIRAGE
 
@@ -166,42 +183,59 @@ try:
 
 
 
+
+
         # =====================
-        # VIRAGE DROITE
+        # DROITE
         # =====================
 
         elif etat == (0,1,1):
+
 
             compteur_perdu = 0
 
             compteur_virage += 1
 
+
             dernier_sens = -1
+
 
 
             if compteur_virage > 2:
 
+
                 cible = DROITE_FORT
+
                 vitesse = VITESSE_VIRAGE
+
 
             else:
 
+
                 cible = DROITE_LEGER
+
                 vitesse = VITESSE_APPROCHE
+
+
 
 
 
 
         elif etat == (0,0,1):
 
+
             compteur_perdu = 0
 
+
             compteur_virage += 2
+
 
             dernier_sens = -1
 
 
+
             cible = DROITE_FORT
+
 
             vitesse = VITESSE_VIRAGE
 
@@ -210,8 +244,9 @@ try:
 
 
 
+
         # =====================
-        # POINTILLES OU PERTE
+        # POINTILLES
         # =====================
 
         elif etat == (0,0,0):
@@ -221,25 +256,24 @@ try:
 
 
 
-            # =====================
-            # POINTILLES
-            # =====================
-            # environ 0.75s
-            # on garde le robot DROIT
+            # petit blanc = pointillé
 
-            if compteur_perdu < 30:
+            if compteur_perdu < 35:
 
 
-                cible = CENTRE
+                # IMPORTANT :
+                # garder exactement
+                # la trajectoire
 
-                vitesse = 26
+                cible = angle_actuel
+
+
+                vitesse = VITESSE_POINTILLE
 
 
 
 
-            # =====================
-            # VRAIE PERTE
-            # =====================
+            # vraie perte
 
             else:
 
@@ -271,6 +305,8 @@ try:
 
 
 
+
+
         # =====================
         # ACTION
         # =====================
@@ -285,18 +321,13 @@ try:
 
 
 
-
-        # mémoire dernière ligne vue
-
         if etat != (0,0,0):
 
             dernier_etat = etat
 
 
 
-
         time.sleep(0.025)
-
 
 
 
@@ -311,4 +342,7 @@ except KeyboardInterrupt:
     robot.stopper()
 
 
-    servos.set_angle(0,CENTRE)
+    servos.set_angle(
+        0,
+        CENTRE
+    )
